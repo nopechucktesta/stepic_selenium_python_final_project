8cc0a057aa5584a3a55edf7cce654b978ff4cf39
alexander <afanasyev.alexander@gmail.com>
2019-06-28 10:19:22 +0300

    Introduce ProductPage

---
+++ pages/product_page.py
@@ -0,0 +1,9 @@
+from .base_page import BasePage
+
+
+class ProductPage(BasePage):
+    def should_be_product_page(self):
+        raise Exception("Not implemented yet")
+
+    def add_to_basket(self):
+        raise Exception("Not implemented yet")
