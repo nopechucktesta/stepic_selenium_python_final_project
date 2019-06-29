b0a754dfc494bd71d9f7536f4714f609d2bbf176
alexander <afanasyev.alexander@gmail.com>
2019-06-29 11:36:43 +0300

    Introduce add_to_basket ProductPage method

--- pages/locators.py
+++ pages/locators.py
@@ -11,5 +11,10 @@ class MainPageLocators(object):
 
 
 class ProductPageLocators(object):
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form .btn-add-to-basket")
     PRODUCT_NAME = (By.CSS_SELECTOR, "article.product_page .product_main h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, "article.product_page .product_main .price_color")
+    BASKET_TOTAL_UPDATE_MESSAGE_VALUE = (
+        By.XPATH, "//div[@class='alertinner ' and contains(string(), 'Your basket total is now')]//strong")
+    HAS_BEEN_ADDED_TO_BASKET_MESSAGE_PRODUCT_NAME = (
+        By.XPATH, "//div[@class='alertinner ' and contains(string(), 'has been added to your basket')]/strong")
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,16 +1,32 @@
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
 from .base_page import BasePage
 from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
-    def should_be_product_page(self):
-        raise Exception("Not implemented yet")
-
     def add_to_basket(self):
-        raise Exception("Not implemented yet")
+        self.add_to_basket_button().click()
+        self.solve_quiz_and_get_code()
+        product_has_been_added_message = self.wait_for_product_has_been_added_message()
+        basket_total_update_message = self.wait_for_basket_total_update_message()
+        assert product_has_been_added_message.text == self.product_name(), "Wrong added product name"
+        assert basket_total_update_message.text == self.product_price(), "Wrong basket total"
+
+    def add_to_basket_button(self):
+        return self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
 
     def product_name(self):
         return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
 
     def product_price(self):
         return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+
+    def wait_for_product_has_been_added_message(self):
+        return WebDriverWait(self.browser, 5).until(expected_conditions.presence_of_element_located(
+            ProductPageLocators.HAS_BEEN_ADDED_TO_BASKET_MESSAGE_PRODUCT_NAME))
+
+    def wait_for_basket_total_update_message(self):
+        return WebDriverWait(self.browser, 5).until(expected_conditions.presence_of_element_located(
+            ProductPageLocators.BASKET_TOTAL_UPDATE_MESSAGE_VALUE))
